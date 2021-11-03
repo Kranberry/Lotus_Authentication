0b359c5b5a6f1bcc8f6429c3cca2904d04c10176
Kranberry <60517786+Kranberry@users.noreply.github.com>
2021-11-03 17:37:48 +0100

    Changed root firectory of project

---
+++ Lotus_Authentication_Tests/ApiKeyTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using Lotus_Authentication.Models;
+
+namespace Lotus_Authentication_Tests;
+
+[TestFixture]
+internal class ApiKeyTests
+{
+    [Test]
+    public void Setup()
+    {
+
+    }
+
+    public void IsValidApiKeyTest(ApiKey apiKey)
+    {
+
+    }
+}
