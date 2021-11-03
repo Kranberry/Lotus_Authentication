0235154f3cbdf2a6d22d66785acab69b72319872
Kranberry <60517786+Kranberry@users.noreply.github.com>
2021-11-03 17:31:13 +0100

    Added base for email validaton

---
+++ Data/EmailValidator.cs
@@ -0,0 +1,13 @@
+﻿namespace Lotus_Authentication.Data;
+
+public class EmailValidator
+{
+    public static bool IsValidEmail(string email)
+    {
+        if (string.IsNullOrWhiteSpace(email))
+            return false;
+
+        throw new NotImplementedException();
+    }
+}
+
