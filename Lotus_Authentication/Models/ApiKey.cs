e1630d98b58f550ebd991c45eacd3493c7b16fd4
Kranberry <60517786+Kranberry@users.noreply.github.com>
2021-11-03 17:39:06 +0100

    wow? git commit -m wow? git commit -m wow?

---
+++ Lotus_Authentication/Models/ApiKey.cs
@@ -0,0 +1,16 @@
+﻿namespace Lotus_Authentication.Models;
+
+public class ApiKey
+{
+
+
+    public static string GenerateApiKey()
+    {
+        throw new NotImplementedException();
+    }
+
+    public static bool IsValidApiKey(ApiKey apiKey)
+    {
+        throw new NotImplementedException();
+    }
+}
