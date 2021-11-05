39f515e0c53bef59411e5b530e4f51ed40cfabde
Kranberry <60517786+Kranberry@users.noreply.github.com>
2021-11-05 23:10:21 +0100

    asd

--- Lotus_Authentication/API/ApiModels/User.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Lotus_Authentication.API.ApiModels;
-
-public class User
-{
-    public string? UserName { get; set; }
-    public string Password { get; set; }    // Non nullable will be required for the API request body
-    public string? Email { get; set; }  // Nullable will be optional for the API request body
-}
--- Lotus_Authentication/GlobalUsings.cs
+++
@@ -1,1 +0,0 @@
-﻿global using Lotus_Authentication.Data;
