6dedbe85830065a77c911f6b789fbd086339303a
Kranberry <60517786+Kranberry@users.noreply.github.com>
2021-11-06 18:23:13 +0100

    table now good

--- SqlScripts/Tables/create_system_logs.sql
+++ SqlScripts/Tables/create_system_logs.sql
@@ -1,10 +1,11 @@
 CREATE TABLE system_logs
 (
-    [system_logs_id] INT PRIMARY KEY IDENTITY(1, 1) NOT NULL,
-    [severity] VARCHAR(MAX) NOT NULL,
-    [exception_type] VARCHAR(MAX),
-    [message] VARCHAR(MAX),
-    [page] VARCHAR(MAX),
-    [stacktrace] VARCHAR(MAX),
-    [entered_date] TIMESTAMP WITHOUT TIME ZONE DEFAULT NOW()::TIMESTAMP
+    "system_logs_id" SERIAL PRIMARY KEY NOT NULL,
+    "application" TEXT NOT NULL,
+    "severity" TEXT NOT NULL,
+    "exception_type" TEXT,
+    "message" TEXT,
+    "page" TEXT,
+    "stacktrace" TEXT,
+    "entered_date" TIMESTAMP WITHOUT TIME ZONE DEFAULT NOW()::TIMESTAMP
 )
