b7cdc1a8225c996c056744250b38e404afed78ab
Kranberry <60517786+Kranberry@users.noreply.github.com>
2021-11-07 17:52:47 +0100

    6

--- Lotus_Authentication/Data/Enums.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Lotus_Authentication.Data;
-
-public enum LogSeverity
-{
-    Informational = 1,
-    Warning = 2,
-    Error = 3,
-    Fatal = 4,
-}
--- Lotus_Authentication/GlobalUsings.cs
+++
@@ -1,3 +0,0 @@
-﻿global using Lotus_Authentication.Data;
-global using Lotus_Authentication.API.ApiModels;
-global using Lotus_Authentication.API;
