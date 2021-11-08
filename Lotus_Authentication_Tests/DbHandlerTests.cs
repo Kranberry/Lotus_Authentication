8185f42edce99b904f2198eaad8d4ccca3c633dd
Kranberry <60517786+Kranberry@users.noreply.github.com>
2021-11-08 09:37:55 +0100

    SyslogAdd test

---
+++ Lotus_Authentication_Tests/DbHandlerTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using Lotus_Authentication.Data;
+using System.Threading.Tasks;
+using System;
+
+namespace Lotus_Authentication_Tests;
+
+[TestFixture]
+internal class DbHandlerTests
+{
+    [SetUp]
+    public void Setup()
+    {
+
+    }
+    
+    [Test]
+    public async ValueTask SysLogAddTestSuccess()
+    {
+        int result = await DbHandler.AddNewSystemLog(LogSeverity.Informational
+                                                    , null
+                                                    , "Automatic unit test"
+                                                    , $"TestClass: {nameof(DbHandlerTests)}, TestMethod: {nameof(SysLogAddTestSuccess)}");
+        int expected = 1;
+        Assert.AreEqual(expected, result);
+    }
+}
