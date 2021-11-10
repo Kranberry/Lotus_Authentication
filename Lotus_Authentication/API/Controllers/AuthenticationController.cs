e2eefabc455362e489cfcc9c237edb1be7484a25
Kranberry <60517786+Kranberry@users.noreply.github.com>
2021-11-10 15:58:56 +0100

    Authentication api call. Not tested. Test after being able to add new user

--- Lotus_Authentication/API/Controllers/AuthenticationController.cs
+++ Lotus_Authentication/API/Controllers/AuthenticationController.cs
@@ -10,23 +10,31 @@ namespace Lotus_Authentication.API.Controllers
 	{
 		// GET api/<AuthenticationController>/user
 		[HttpGet, Route("user")]
-		public ActionResult<User> Get([FromBody] User body)
+		public ActionResult<User> Get([FromBody] Dictionary<string, string> body)
 		{
+#warning Test this after new user creation has been made
 			/* {
 			 *	  "username": "testsson",	// optional, but only if email is not supplied
 			 *	  "email": "testsson@email.com",	// optional, but only if username is not supplied
 			 *	  "password": "C6B77501AF2051430FDCE1659E8A9582CCBA40CA"	// Required SHA1 validated
 			 * }
 			 */
-			if(string.IsNullOrWhiteSpace(body.UserName) && string.IsNullOrWhiteSpace(body.Email))
-				return BadRequest("The 'email' and 'username' fields can not be empty");
-			if (!SHA1Hash.IsValidSHA1(body.Password))
-                return BadRequest("The 'password' field is not a valid SHA1 checksum");
+			body.TryGetValue("userName", out string? userName);
+			body.TryGetValue("email", out string? email);
+			body.TryGetValue("password", out string? password);
+			if(email is not null && !EmailValidator.IsValidEmail(email))
+				return new BadRequestObjectResult($"{email} is not a valid email address");
+			if (string.IsNullOrWhiteSpace(password))
+				return new BadRequestObjectResult("password cannot be null or empty");
+			if(SHA1Hash.IsValidSHA1(password))
+				return new BadRequestObjectResult("password must be sent in as a SHA1 checksum");
 
-			if (!EmailValidator.IsValidEmail(body.Email))
-				return BadRequest("Input email is not a valid email address");
+			User? user = DbHandler.GetUser(userName, email, password);
 
-            return body;
+			if (user is null)
+				return new BadRequestResult();
+
+			return user;
 		}
 	}
 }
