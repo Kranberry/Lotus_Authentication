62f290a2db783a478c649f30ea58e504d6c3d199
Kranberry <60517786+Kranberry@users.noreply.github.com>
2021-11-21 18:24:03 +0100

    Added SHA1 exception

--- Lotus_Authentication/Data/SHA1Hash.cs
+++ Lotus_Authentication/Data/SHA1Hash.cs
@@ -2,29 +2,39 @@
 using System.Text.RegularExpressions;
 using XSystem.Security.Cryptography;
 
-namespace Lotus_Authentication.Data
+namespace Lotus_Authentication.Data;
+
+public class SHA1Hash
 {
-	public class SHA1Hash
-	{
-        public static string Hash(string input)
-        {
-			using SHA1Managed sha1 = new();
+    public static string Hash(string input)
+    {
+		using SHA1Managed sha1 = new();
 
-			byte[] hash = sha1.ComputeHash(Encoding.UTF8.GetBytes(input));
-			StringBuilder sb = new(hash.Length * 2);
+		byte[] hash = sha1.ComputeHash(Encoding.UTF8.GetBytes(input));
+		StringBuilder sb = new(hash.Length * 2);
 
-			foreach (byte b in hash)
-			{
-				sb.Append(b.ToString("X2"));
-			}
-
-			return sb.ToString();
-		}
-
-		public static bool IsValidSHA1(string str)
+		foreach (byte b in hash)
 		{
-			Regex regex = new(@"^[a-fA-F0-9]{40}$");
-			return regex.IsMatch(str);
+			sb.Append(b.ToString("X2"));
 		}
+
+		return sb.ToString();
+	}
+
+	public static bool IsValidSHA1(string str)
+	{
+		Regex regex = new(@"^[a-fA-F0-9]{40}$");
+		return regex.IsMatch(str);
+	}
+}
+
+/// <summary>
+/// Throw whenever a string must be a valid SHA1 checksum but is not
+/// </summary>
+public class BadSHA1ReferenceException : BaseException
+{
+	public BadSHA1ReferenceException(LogSeverity severity, string message, string page)
+	{
+		SendSyslog(severity, message, page, this);
 	}
 }
