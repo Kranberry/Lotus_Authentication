9b178f54d6953e9770f41213d79cfecba20987da
Kranberry <60517786+Kranberry@users.noreply.github.com>
2022-01-15 12:22:29 +0100

    Appconfig duuuud

---
+++ Lotus_Authentication/Data/AppConfig.cs
@@ -0,0 +1,12 @@
+﻿namespace Lotus_Authentication.Data;
+
+internal static class AppConfig
+{
+    private static IConfiguration _Configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json", true).Build();
+
+    private static string GetValueOfKey(string key) => _Configuration[key];
+
+    internal static string SkeletonKey => GetValueOfKey("secrets:skeleton-key");
+    internal static string SkeletonKeyID => GetValueOfKey("secrets:skeleton-key-id");
+    internal static string ActiveDatabaseCS => GetValueOfKey("authenticator");
+}
