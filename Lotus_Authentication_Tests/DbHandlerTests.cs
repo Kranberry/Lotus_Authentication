33e2e180508642b47c9e7689bc2a5ad071eef473
Kranberry <60517786+Kranberry@users.noreply.github.com>
2022-03-26 18:17:15 +0100

    6

--- Lotus_Authentication_Tests/DbHandlerTests.cs
+++ Lotus_Authentication_Tests/DbHandlerTests.cs
@@ -26,20 +26,4 @@ internal class DbHandlerTests
         int expected = 1;
         Assert.AreEqual(expected, result);
     }
-
-    [Test]
-    public void InsertSucceedMemberTest()
-    {
-        // TODO: Generate a test case from a username and email address that is sure to not exist. And then delete the user when done.
-        User user = new(0, "test", "testsson", "testaren@test.com", "testarn", UserType.Regular, Gender.Male, "SE", null, null, DateTime.Now, null, false);
-        Assert.DoesNotThrowAsync(async () => await DbHandler.InsertUser(user, "2527C-ACAd56BA-F08cBbFE-e8cdbFef-EA0c7Ab1"));
-    }
-
-    [Test]
-    public void InsertFailMemberTest()
-    {
-        //TODO: Add more test cases
-        User user = new(0, "test", "testsson", "testaren@test.com", "testarn", UserType.Regular, Gender.Male, "SE", null, null, DateTime.Now, null, false);
-        Assert.ThrowsAsync<BadApiKeyReferenceException>(async () => await DbHandler.InsertUser(user, "2527C-ACAd56BA-F08cBbFE-e8cbFef-EA0c71"));
-    }
 }
