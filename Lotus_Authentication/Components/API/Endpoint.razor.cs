5e71378fedfecc036aec7933068fae0c447d83c0
Kranberry <60517786+Kranberry@users.noreply.github.com>
2022-04-15 23:02:30 +0200

    Created Endpoint component

---
+++ Lotus_Authentication/Components/API/Endpoint.razor.cs
@@ -0,0 +1,21 @@
+﻿namespace Lotus_Authentication.Components;
+
+public partial class Endpoint
+{
+    private string GetStatusClass(int statusCode) => statusCode switch
+    {
+        (>= 200 and < 300) => "alert alert-success",
+        (>= 300 and < 400) => "alert alert-warning",
+        (>= 400 and < 500) => "alert alert-danger",
+        _ => "alert alert-info"
+    };
+
+    private string GetMethodClass(string method) => method switch
+    {
+        "POST" => "badge bg-primary",
+        "DELETE" => "badge bg-danger",
+        "GET" => "badge bg-info text-dark",
+        "PUT" => "badge bg-primary",
+        _ => "badge bg-dark",
+    };
+}
